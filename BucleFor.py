90de9db859409c9f4c70bc3baa509daf1a0c65aa
alumnos alumnos <alumnos@new.edu.ar>
2024-06-10 15:33:10 -0300

    Mi primer commit

---
+++ BucleFor.py
@@ -0,0 +1,20 @@
+##Mi primer bucle
+a=2
+for i in range (1,11):
+    print(f"{a}X{i} =")
+    print(i*a)
+
+
+a=3
+for i in range(1,11):
+     print(f"{a}x{i}=")
+     print(i*a)
+
+
+
+a=6
+for i in range (1,11):
+     print(f"{a}X{i}=")
+     print(i*a)
+
+
