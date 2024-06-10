4c8a7d2aaea8a476e10393f59f5cfba99a3e3b93
alumnos alumnos <alumnos@new.edu.ar>
2024-06-10 15:31:15 -0300

    mi primer commit

---
+++ EJERCICIOS.py
@@ -0,0 +1,40 @@
+
+##EJERICIO 1
+
+a = int(input('Dame tu edad'))
+a= 18
+if a >18:
+    print("mayor de edad")
+else:
+    print("menor de edad")
+
+
+##EJERCICIO 2:
+
+a= int(input('Introduzca una contraseña'))
+if a == "contraseña" 
+    print ("correcto")
+else: 
+    print("contraseña incorrecta")
+
+
+##EJERCICIO 3:
+a= int(input("Dame un numero"))
+b= int(input("Dame otro numero"))
+if a !=0: 
+    print(b/a)
+else:
+    print ("error")
+
+
+##EJERCICIO 4
+a= int(input("Dame un número entero positivo"))
+while a < 0:
+    print("dame otro nímero que sea entero y positivo")
+
+ 
+   if a>0
+    
+    
+
+
